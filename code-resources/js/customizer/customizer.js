17b921906cad98d71fe1114bb50c63e81fe37af7
cjtermini <figmentdragon@protonmail.com>
2022-04-24 21:51:59 -0400

    update backup

--- code-resources/js/customizer/customizer.js
+++ code-resources/js/customizer/customizer.js
@@ -1,7 +1,4 @@
-/* global wp, jQuery */
 /**
- * File customizer.js.
- *
  * Theme Customizer enhancements for a better user experience.
  *
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
@@ -19,24 +16,21 @@
 			$( '.site-description' ).text( to );
 		} );
 	} );
-
 	// Header text color.
 	wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
 				$( '.site-title, .site-description' ).css( {
-					clip: 'rect(1px, 1px, 1px, 1px)',
-					position: 'absolute',
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
 				} );
 			} else {
 				$( '.site-title, .site-description' ).css( {
-					clip: 'auto',
-					position: 'relative',
-				} );
-				$( '.site-title a, .site-description' ).css( {
-					color: to,
+					'clip': 'auto',
+					'color': to,
+					'position': 'relative'
 				} );
 			}
 		} );
 	} );
-}( jQuery ) );
+} )( jQuery );
